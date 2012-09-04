a2e131923840cc2c96b6350b5b554da2c9bb8d1b
Andreas Maunz <andreas@maunz.de>
2012-09-04 10:27:11 +0200

    Added pkg-config support

--- ext/openbabel/extconf.rb
+++ ext/openbabel/extconf.rb
@@ -35,7 +35,10 @@ begin
   Dir.chdir ob_bindings_dir do
     puts "Compiling and instaling OpenBabel Ruby bindings."
     `sed -i 's/Init_OpenBabel/Init_openbabel/g' *cpp`
-    require './extconf.rb'
+    # get include and lib from pkg-config
+    ob_include=`pkg-config openbabel-2.0 --cflags-only-I`.sub(/\s+/,'').sub(/-I/,'')
+    ob_lib=`pkg-config openbabel-2.0 --libs-only-L`.sub(/\s+/,'').sub(/-L/,'')
+    `ruby extconf.rb --with-openbabel-include=#{ob_include} --with-openbabel-lib=#{ob_lib}`
     `sed -i 's/-flat_namespace//' Makefile`
     puts `make`
   end
